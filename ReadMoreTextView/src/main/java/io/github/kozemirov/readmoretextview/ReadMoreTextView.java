8aca703ce53b063ed3459cfe3bf3dc7b831baa78
pavel <pskozemirov@gmail.com>
2022-06-16 14:30:18 +0300

    Build 1.0.4

--- ReadMoreTextView/src/main/java/io/github/kozemirov/readmoretextview/ReadMoreTextView.java
+++ ReadMoreTextView/src/main/java/io/github/kozemirov/readmoretextview/ReadMoreTextView.java
@@ -2,7 +2,10 @@ package io.github.kozemirov.readmoretextview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -17,9 +20,13 @@ public class ReadMoreTextView extends LinearLayoutCompat {
     boolean hide = true;
     int lineLimit = 4;
     String text;
-
+    Float textSize;
+    int textColor;
     TextView des;
     TextView button;
+    int justification;
+    String showLessText;
+    String readMoreText;
 
     public ReadMoreTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -28,25 +35,46 @@ public class ReadMoreTextView extends LinearLayoutCompat {
                               .obtainStyledAttributes(attrs, R.styleable.ReadMoreTextView, 0, 0);
         try {
             text = a.getString(R.styleable.ReadMoreTextView_text);
+            showLessText = a.getString(R.styleable.ReadMoreTextView_showLessText);
+            readMoreText = a.getString(R.styleable.ReadMoreTextView_readMoreText);
+            text = a.getString(R.styleable.ReadMoreTextView_text);
             hide = a.getBoolean(R.styleable.ReadMoreTextView_hidden, true);
             lineLimit = a.getInteger(R.styleable.ReadMoreTextView_limit, 4);
+            textSize = a.getFloat(R.styleable.ReadMoreTextView_textSize, 12);
+            textColor = a.getColor(R.styleable.ReadMoreTextView_color, Color.GRAY);
+            justification = a.getInteger(R.styleable.ReadMoreTextView_justificationMode, 1);
         } finally {
             a.recycle();
         }
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View parent = inflater.inflate(R.layout.read_more_text_view, this, true);
+
+        if (showLessText == null)
+            showLessText = context.getString(R.string.show_less);
+        if (readMoreText == null)
+            readMoreText = context.getString(R.string.read_more);
+
+
         des = parent.findViewById(R.id.description);
         button = parent.findViewById(R.id.button);
 
-        des.setMaxLines(MAX_LINES);
         des.setText(text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            des.setJustificationMode(justification);
+        }
+        des.setMaxLines(MAX_LINES);
+        des.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+        des.setTextColor(textColor);
+
+        button.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+        button.setTextColor(textColor);
 
         if (hide) {
             des.setMaxLines(lineLimit);
-            button.setText(R.string.read_more);
+            button.setText(readMoreText);
         } else {
-            button.setText(R.string.show_less);
+            button.setText(showLessText);
         }
 
         button.setOnClickListener((View v) -> {
